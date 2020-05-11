c412870f1e077d779dedf07827c16cf827ce4d10
Sara Mirzaee <sara.mirzaee@rsmas.miami.edu>
2020-05-11 18:36:07 -0500

    fix bug in stack.py for new argument rmFilter

--- contrib/stack/topsStack/Stack.py
+++ contrib/stack/topsStack/Stack.py
@@ -223,7 +223,10 @@ class config(object):
         self.f.write('defomax : ' + self.defoMax + '\n')
         self.f.write('rlks : ' + self.rangeLooks + '\n')
         self.f.write('alks : ' + self.azimuthLooks + '\n')
-        self.f.write('rmfilter : ' + self.rmFilter + '\n')
+        if self.rmFilter:
+            self.f.write('rmFilter : True \n')
+        else:
+            self.f.write('rmFilter : False\n')
         self.f.write('method : ' + self.unwMethod + '\n')
 
     def unwrapSnaphu(self, function):
